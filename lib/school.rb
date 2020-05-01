ff25a039c0ba73863da71bc785a62a487f0f148c
Christine Zosche <christine.zosche@gmail.com>
2020-05-01 17:03:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,12 +5,12 @@ class School
     @roster = {}
   end
   
-  def add_student(student_name, grade)
+  def add_student(name, grade)
       if @roster[grade]
-        @roster[grade] << student_name
+        @roster[grade].<<(name)
       else
         @roster[grade] = []
-        @roster[grade] << student_name
+        @roster[grade] << name
       end
   end
 end
