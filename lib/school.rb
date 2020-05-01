5d3d248257e9b49022221e2cc2b7ad8f1146d40f
Christine Zosche <christine.zosche@gmail.com>
2020-05-01 16:41:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,6 @@ class School
   roster = {}
   
   def add_student(student_name, grade)
-      roster[grade] = []
-      roster[grade] << "#{student_name}"
+      roster => {grade => ["#{student_name}]"]}
   end
 end
