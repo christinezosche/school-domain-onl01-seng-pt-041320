3c15b0bf3791683e9ed98b9e320fabc66a118d22
Christine Zosche <christine.zosche@gmail.com>
2020-05-01 16:31:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,5 +7,10 @@ class School
   roster = {}
   
   def add_student(name, grade)
-    roster[:grade] = "#{name}"
+    if roster.include?(:grade)
+      roster[:grade} << "#{name}"
+    else
+      roster[:grade] = []
+      roster[:grade} << "#{name}"
+    end
 end
